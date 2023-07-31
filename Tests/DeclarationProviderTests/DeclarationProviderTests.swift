fcfc38b1987fd22264435b1c2f911353b2e04cc8
Akivili Collindort <isoblack1@outlook.com>
2023-07-31 18:02:36 +0800

    Revert "Revert "Merge branch 'main' of https://github.com/AreroKetahi/SwiftyDeclarationProvider""
    This reverts commit bae744ef787ca4126d903eaa75bc58f4e9ff6afd.

--- Tests/DeclarationProviderTests/DeclarationProviderTests.swift
+++ Tests/DeclarationProviderTests/DeclarationProviderTests.swift
@@ -13,6 +13,25 @@ final class DeclarationProviderTests: XCTestCase {
                 DGroup {
                     DText("hello world")
                 }
+                DText("1234")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
+                DText("1456")
             }
             IndentDGroup {
                 DTitle("Test")
